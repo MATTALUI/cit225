065b6e1c14abafb85f2935b32de94693379a90c4
Matthew Hummer <biggreyson@gmail.com>
2020-09-21 10:50:54 -0600

    change lab2 formatting

--- lab2/apply_oracle_lab2.sql
+++ lab2/apply_oracle_lab2.sql
@@ -9,7 +9,7 @@
 --  Change Date    Change Reason
 -- -------------  ---------------------------------------------------
 --  13-Aug-2019    Incorporate diagnostic scripts.
---  
+--
 -- ------------------------------------------------------------------
 -- This creates tables, sequences, indexes, and constraints necessary
 -- to begin lesson #3. Demonstrates proper process and syntax.
@@ -47,75 +47,116 @@ SPOOL apply_oracle_lab2.txt
 -- ------------------------------------------------------------------
 
 -- STEP 1  ----------------------------------------------------------
-SELECT account_number, credit_card_number
-FROM member;
+COL account_number        FORMAT A12    HEADING "Account|Number"
+COL credit_card_number    FORMAT A22    HEADING "Credit|Card Number"
+
+SELECT
+        account_number,
+        credit_card_number
+FROM    member;
 
 -- STEP 2  ----------------------------------------------------------
 COL first_name   FORMAT A12  HEADING "First Name"
 COL middle_name  FORMAT A12  HEADING "Middle Name"
 COL last_name    FORMAT A12  HEADING "Last Name"
-SELECT first_name, middle_name, last_name
-FROM contact;
+
+SELECT
+          first_name,
+          middle_name,
+          last_name
+FROM      contact
+ORDER BY  last_name;
 
 -- STEP 3  ----------------------------------------------------------
 COL city            FORMAT A14  HEADING "City"
 COL state_province  FORMAT A8   HEADING "State|Province"
 COL postal_code     FORMAT A6   HEADING "Postal|Code"
-SELECT city, state_province, postal_code
-FROM address;
+
+SELECT
+        city,
+        state_province,
+        postal_code
+FROM    address;
 
 -- STEP 4  ----------------------------------------------------------
 COL check_out_date  FORMAT A12  HEADING "Check Out|Date"
 COL return_date     FORMAT A12  HEADING "Return|Date"
-SELECT check_out_date, return_date
-FROM rental;
+
+SELECT
+        check_out_date,
+        return_date
+FROM    rental;
 
 -- STEP 5  ----------------------------------------------------------
 COL item_title         FORMAT A30  HEADING "Item Title"
-COL item_rating        FORMAT A5   HEADING "Item|Rating"
+COL item_rating        FORMAT A10  HEADING "Item|Rating"
 COL item_release_date  FORMAT A12  HEADING "Item|Release|Date"
-SELECT item_title, item_rating, item_release_date
-FROM item;
+
+SELECT
+        item_title,
+        item_rating,
+        item_release_date
+FROM    item;
 
 -- STEP 6  ----------------------------------------------------------
 COL item_title         FORMAT A30  HEADING "Item Title"
-COL item_rating        FORMAT A5   HEADING "Item|Rating"
+COL item_rating        FORMAT A15  HEADING "Item|Rating"
 COL item_release_date  FORMAT A12  HEADING "Item|Release|Date"
-SELECT item_title, item_rating, item_release_date
-FROM item
-WHERE item_rating = 'PG'
-ORDER BY item_title ASC;
+
+SELECT
+          item_title,
+          item_rating,
+          item_release_date
+FROM      item
+WHERE     item_rating = 'PG'
+ORDER BY  item_title;
 
 -- STEP 7  ----------------------------------------------------------
 COL first_name   FORMAT A12  HEADING "First Name"
 COL middle_name  FORMAT A12  HEADING "Middle Name"
 COL last_name    FORMAT A12  HEADING "Last Name"
-SELECT first_name, middle_name, last_name
-FROM contact
-WHERE last_name = 'Sweeny'
-AND middle_name IS NULL;
+
+SELECT
+        first_name,
+        middle_name,
+        last_name
+FROM    contact
+WHERE   last_name = 'Sweeney'
+AND     middle_name IS NULL;
 
 -- STEP 8  ----------------------------------------------------------
 COL first_name   FORMAT A12  HEADING "First Name"
 COL last_name    FORMAT A12  HEADING "Last Name"
-SELECT first_name, middle_name, UPPER(last_name)
-FROM contact
-WHERE last_name LIKE UPPER('viz%');
+
+SELECT
+        first_name,
+        middle_name,
+        last_name
+FROM    contact
+WHERE   UPPER(last_name) LIKE UPPER('ViZ%');
+-- Made ViZ weird cased so I know the case insesitivity works as expected
 
 -- STEP 9  ----------------------------------------------------------
 COL city            FORMAT A14  HEADING "City"
 COL state_province  FORMAT A8   HEADING "State|Province"
 COL postal_code     FORMAT A6   HEADING "Postal|Code"
-SELECT city, state_province, postal_code
-FROM address
-WHERE city IN ('Provo', 'San Jose');
+
+SELECT
+        city,
+        state_province,
+        postal_code
+FROM    address
+WHERE   city IN ('Provo', 'San Jose');
 
 -- STEP 10 ----------------------------------------------------------
 COL item_title         FORMAT A30  HEADING "Item Title"
 COL item_release_date  FORMAT A12  HEADING "Item|Release|Date"
-SELECT item_title, item_release_date
-FROM item
-WHERE item_release_date BETWEEN '01-JAN-2003' AND '31-DEC-2003';
+
+SELECT
+        item_title,
+        item_release_date
+FROM    item
+WHERE   item_release_date BETWEEN '01-JAN-2003' AND '31-DEC-2003';
 
 -- ------------------------------------------------------------------
 -- Enter lab code above.
