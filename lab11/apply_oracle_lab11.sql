2e7023f6504516e9a680f038bfe3889372f60db7
Student <biggreyson@gmail.com>
2020-11-24 19:34:59 -0700

    lab 11 4/5

--- lab11/apply_oracle_lab11.sql
+++ lab11/apply_oracle_lab11.sql
@@ -27,7 +27,7 @@ SPOOL apply_oracle_lab11.txt
 -- --------------------------------------------------------
 --  Step #1 : Merge statement to the rental table.
 -- --------------------------------------------------------
-
+/*
 -- Count rentals before insert.
 SELECT   COUNT(*) AS "Rental before count"
 FROM     rental;
@@ -211,15 +211,14 @@ INSERT VALUES
 , source.created_by
 , source.creation_date
 , source.last_updated_by
-,source.last_update_date);
+, source.last_update_date);
 
 
 -- Count transactions after insert
 SELECT   COUNT(*)
 FROM     transaction;
 
-SPOOL OFF
-
+*/
 -- --------------------------------------------------------
 --  Step #4(a) : Put merge statements in a procedure.
 -- --------------------------------------------------------
@@ -232,7 +231,26 @@ BEGIN
 
   -- Insert or update the table, which makes this rerunnable when the file hasn't been updated.
   MERGE INTO rental target
-  USING ( ... query_statement ... ) source
+  USING (
+  	SELECT   	DISTINCT
+	         	r.rental_id,
+	        	c.contact_id,
+	        	tu.check_out_date AS check_out_date,
+	        	tu.return_date AS return_date,
+	        	1001 AS created_by,
+	        	TRUNC(SYSDATE) AS creation_date,
+	        	1001 AS last_updated_by,
+	        	TRUNC(SYSDATE) AS last_update_date
+	FROM     	member m INNER JOIN contact c
+	ON       	m.member_id = c.member_id INNER JOIN transaction_upload tu
+	ON       	c.first_name = tu.first_name
+	AND      	NVL(c.middle_name,'x') = NVL(tu.middle_name,'x')
+	AND      	c.last_name = tu.last_name
+	LEFT JOIN	rental r
+	ON		r.customer_id = c.contact_id
+	AND		TRUNC(tu.return_date) = TRUNC(r.return_date)
+	AND		TRUNC(tu.check_out_date) = TRUNC(r.check_out_date)
+  ) source
   ON (target.rental_id = source.rental_id)
   WHEN MATCHED THEN
   UPDATE SET last_updated_by = source.last_updated_by
@@ -240,49 +258,110 @@ BEGIN
   WHEN NOT MATCHED THEN
   INSERT VALUES
   ( rental_s1.NEXTVAL
-  , source.column_name
-  ...
-  , source.column_name)) source
-  ON (target.rental_id = source.rental_id)
-  WHEN MATCHED THEN
-  UPDATE SET last_updated_by = source.last_updated_by
-  ,          last_update_date = source.last_update_date
-  WHEN NOT MATCHED THEN
-  INSERT VALUES
-  ( rental_s1.nextval
-  ...
+  , source.contact_id -- NOT THE NAME ON THE TABLE; COULD BE PROBLEMATIC
+  , source.check_out_date
+  , source.return_date
+  , source.created_by
+  , source.creation_date
+  , source.last_updated_by
   , source.last_update_date);
 
   -- Insert or update the table, which makes this rerunnable when the file hasn't been updated.
   MERGE INTO rental_item target
-  USING ( ... query_statement ... ) source
+  USING (
+	SELECT   ri.rental_item_id
+	,        r.rental_id
+	,        tu.item_id
+	,        TRUNC(r.return_date) - TRUNC(r.check_out_date) AS rental_item_price
+	,        cl.common_lookup_id AS rental_item_type
+	,        1001 AS created_by
+	,        TRUNC(SYSDATE) AS creation_date
+	,        1001 AS last_updated_by
+	,        TRUNC(SYSDATE) AS last_update_date
+	FROM     	member m INNER JOIN contact c
+	ON       	m.member_id = c.member_id INNER JOIN transaction_upload tu
+	ON       	c.first_name = tu.first_name
+	AND      	NVL(c.middle_name,'x') = NVL(tu.middle_name,'x')
+	AND      	c.last_name = tu.last_name
+	LEFT JOIN	rental r
+	ON		r.customer_id = c.contact_id
+	AND		TRUNC(tu.return_date) = TRUNC(r.return_date)
+	AND		TRUNC(tu.check_out_date) = TRUNC(r.check_out_date)
+	JOIN		common_lookup cl
+	ON      	cl.common_lookup_table = 'RENTAL_ITEM'
+	AND     	cl.common_lookup_column = 'RENTAL_ITEM_TYPE'
+	AND     	cl.common_lookup_type = tu.rental_item_type
+	LEFT JOIN	rental_item ri
+	ON		ri.rental_id = r.rental_id
+  ) source
   ON (target.rental_item_id = source.rental_item_id)
   WHEN MATCHED THEN
   UPDATE SET last_updated_by = source.last_updated_by
   ,          last_update_date = source.last_update_date
   WHEN NOT MATCHED THEN
   INSERT VALUES
-  ( rental_item_s1.nextval
-  , source.column_name
-  ...
-  , source.column_name)) source
-  ON (target.rental_item_id = source.rental_item_id)
-  WHEN MATCHED THEN
-  UPDATE SET last_updated_by = source.last_updated_by
-  ,          last_update_date = source.last_update_date
-  WHEN NOT MATCHED THEN
-  INSERT
-  ( rental_item_id
-  ...
-  , last_update_date)
-  VALUES
-  ( rental_item_s1.nextval
-  ...
-  , source.last_update_date);
+  ( rental_item_s1.nextval                                                                                          
+  , source.rental_id                                                                                          
+  , source.item_id                                                                                            
+  , source.created_by                                                                                         
+  , source.creation_date                                                                                     
+  , source.last_updated_by                                                                                    
+  , source.last_update_date                                                                                   
+  , source.rental_item_price                                                                                  
+  , source.rental_item_type
+  );
 
   -- Insert or update the table, which makes this rerunnable when the file hasn't been updated.
   MERGE INTO transaction target
-  USING ( ... query_statement ... ) source
+  USING (
+	SELECT   t.transaction_id
+	,        tu.payment_account_number AS transaction_account
+	,        cl1.common_lookup_id AS transaction_type
+	,        TRUNC(tu.transaction_date) AS transaction_date
+	,       (SUM(tu.transaction_amount) / 1.06) AS transaction_amount
+	,        r.rental_id
+	,        cl2.common_lookup_id AS payment_method_type
+	,        m.credit_card_number AS payment_account_number
+	,        1001 AS created_by
+	,        TRUNC(SYSDATE) AS creation_date
+	,        1001 AS last_updated_by
+	,        TRUNC(SYSDATE) AS last_update_date
+	FROM     	member m INNER JOIN contact c
+	ON       	m.member_id = c.member_id INNER JOIN transaction_upload tu
+	ON       	c.first_name = tu.first_name
+	AND      	NVL(c.middle_name,'x') = NVL(tu.middle_name,'x')
+	AND      	c.last_name = tu.last_name
+	LEFT JOIN	rental r
+	ON		r.customer_id = c.contact_id
+	AND		TRUNC(tu.return_date) = TRUNC(r.return_date)
+	AND		TRUNC(tu.check_out_date) = TRUNC(r.check_out_date)
+	JOIN		common_lookup cl1
+	ON      	cl1.common_lookup_table = 'TRANSACTION'
+	AND     	cl1.common_lookup_column = 'TRANSACTION_TYPE'
+	AND     	cl1.common_lookup_type = tu.transaction_type
+	JOIN		common_lookup cl2
+	ON      	cl2.common_lookup_table = 'TRANSACTION'
+	AND     	cl2.common_lookup_column = 'PAYMENT_METHOD_TYPE'
+	AND     	cl2.common_lookup_type = tu.payment_method_type
+	LEFT JOIN	transaction t
+	ON		t.transaction_account = tu.payment_account_number
+	AND		t.rental_id = r.rental_id
+	AND		t.transaction_type = cl1.common_lookup_id
+	AND		t.transaction_date = tu.transaction_date
+	AND		t.payment_method_type = cl2.common_lookup_id
+	AND		t.payment_account_number = m.credit_card_number
+	GROUP BY t.transaction_id
+	,        tu.payment_account_number
+	,        cl1.common_lookup_id
+	,        tu.transaction_date
+	,        r.rental_id
+	,        cl2.common_lookup_id
+	,        m.credit_card_number
+	,        1001
+	,        TRUNC(SYSDATE)
+	,        1001
+	,        TRUNC(SYSDATE)
+  ) source
   ON (target.transaction_id = source.transaction_id)
   WHEN MATCHED THEN
   UPDATE SET last_updated_by = source.last_updated_by
@@ -290,22 +369,16 @@ BEGIN
   WHEN NOT MATCHED THEN
   INSERT VALUES
   ( transaction_s1.nextval
-  , source.column_name
-  ...
-  , source.column_name) source
-  ON (target.transaction_id = source.transaction_id)
-  WHEN MATCHED THEN
-  UPDATE SET last_updated_by = source.last_updated_by
-  ,          last_update_date = source.last_update_date
-  WHEN NOT MATCHED THEN
-  INSERT
-  ( transaction_id
-  ...
-  , last_update_date)
-  VALUES
-  ( transaction_s1.nextval
   , source.transaction_account
-  ...
+  , source.transaction_type
+  , source.transaction_date
+  , source.transaction_amount
+  , source.rental_id
+  , source.payment_method_type
+  , source.payment_account_number
+  , source.created_by
+  , source.creation_date
+  , source.last_updated_by
   , source.last_update_date);
 
   -- Save the changes.
@@ -336,7 +409,7 @@ COLUMN transaction_count FORMAT 99,999 HEADING "Transaction|Count"
 --  Rental    Item Transaction
 --   Count   Count       Count
 -- ------- ------- -----------
---       8      12           0
+--       8      13           0
 -- ----------------------------------------------
 SELECT   rental_count
 ,        rental_item_count
@@ -358,7 +431,7 @@ EXECUTE upload_transactions;
 --  Rental    Item Transaction
 --   Count   Count       Count
 -- ------- ------- -----------
---   4,689  11,532       4,681
+--   4,689  11,533       4,681
 -- ----------------------------------------------
 SELECT   rental_count
 ,        rental_item_count
@@ -366,7 +439,6 @@ SELECT   rental_count
 FROM    (SELECT COUNT(*) AS rental_count FROM rental) CROSS JOIN
         (SELECT COUNT(*) AS rental_item_count FROM rental_item) CROSS JOIN
         (SELECT COUNT(*) AS transaction_count FROM transaction);
-
 -- --------------------------------------------------------
 --  Step #4(d) : Execute the procedure for the second time.
 -- --------------------------------------------------------
@@ -384,7 +456,7 @@ EXECUTE upload_transactions;
 --  Rental    Item Transaction
 --   Count   Count       Count
 -- ------- ------- -----------
---   4,689  11,532       4,681
+--   4,689  11,533       4,681
 -- ----------------------------------------------
 
 SELECT   rental_count
@@ -394,6 +466,7 @@ FROM    (SELECT COUNT(*) AS rental_count FROM rental) CROSS JOIN
         (SELECT COUNT(*) AS rental_item_count FROM rental_item) CROSS JOIN
         (SELECT COUNT(*) AS transaction_count FROM transaction);
 
+SPOOL OFF
 -- --------------------------------------------------------
 --  Step #5 : Demonstrate aggregation with sorting options.
 -- --------------------------------------------------------
